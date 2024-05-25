fb4b01927e9ba47374e9b339a416ec44fdcde334
John-S-Turay <turayjohns@gmail.com>
2024-05-25 13:05:27 +0200

    UPDATED: "Header file"

---
+++ header.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+    <div class="container">
+        <!-- Brand -->
+        <a class="navbar-brand" href="#">Census Management System</a>
+        
+        <!-- Toggler button for mobile view -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02"
+            aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Navbar links -->
+        <div class="collapse navbar-collapse" id="navbarColor02">
+            <ul class="navbar-nav ml-auto">
+                <!-- Sign in link -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/form/index.php">Sign in</a>
+                </li>
+                <!-- Sign up link -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/form/signup.php">Sign up</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Scripts for jQuery and Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha384-ZvpUoO/+PpRA5g1f64EzkJz+dFgR5Dx6xS3ho5O+pnTcAxV6pm8bxYMjh1LJ9i+3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-MybMaGEToCjX6E6ZK1FAdnBdloEKvh0J9xX2TBN1zRsE6EgGPpbIviy6orVsQXC9" crossorigin="anonymous"></script>
